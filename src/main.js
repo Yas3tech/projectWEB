8c4021adbc7a27d86a0e2e21d489d3ead23cd90c
Yas3tech <eddouksy@gmail.com>
2025-05-05 11:54:34 +0200

    API in de main
    info ophalen van API en een pagina maken "landen" voor de API (regel 19 - 32) + roete maken voor landen page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import Router from './JS/router.mjs'
 import './CSS/style.css'
- 
+
+async function getInfoLanden() { 
+      const resultaat = await fetch('https://restcountries.com/v3.1/all'); // info ophalen van API database
+      const landen = await resultaat.json();
+      return landen;
+  }
 const home = (container) => {
   container.innerHTML = `
   <h1>Home</h1>
   <p>Welkom op de homepage</p>
     <nav>
+      <a href="#/contact">Contact</a>
+      <a href="#/landen">Landen</a>
+    </nav>
+  `;
+};
+ const landen = (container) => {
+const landen =  getInfoLanden();
+  container.innerHTML = `
+  <h1>Landen</h1>
+  <p>Welkom op de landenpage</p>
+  <ul>
+  ${landen.map(land => `<li>${land.name.common}</li>`)}
+  </ul>
+    <nav>
+    <a href="#/">Terug naar home</a>
       <a href="#/contact">Contact</a>
     </nav>
   `;
@@ -33,5 +53,6 @@ const notFound = (container) => {
 const router = new Router({
   '/': home,
   '/contact': contact,
+  '/landen':landen,
   '/404': notFound
 })
